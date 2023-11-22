6ededd36f5b507d434c678271f6f06e292190cca
judonghyun <wn196785@gmail.com>
2023-11-22 12:31:48 +0900

    1122ju

--- YoungCha/Front/vue-project/src/views/RandomMovieListView.vue
+++ YoungCha/Front/vue-project/src/views/RandomMovieListView.vue
@@ -1,20 +1,50 @@
 <template>
-    <div>
-      <h2>랜덤 5개의 영화</h2>
-      <ul>
-        <li v-for="video in videos" :key="video.id">
-          {{ video.title }}
-        </li>
-      </ul>
+  <div>
+    <h1>홈 화면</h1>
+    <div class="movies-container">
+      <div class="movie" v-for="movie in randomMovies" :key="movie.id">
+        <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt="포스터" class="poster">
+        <h2>{{ movie.title }}</h2>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
   
   <script setup>
   import { ref } from 'vue';
 
   const videos = ref([]);
   const props = defineProps(['movies']);
-  
+
+  const searchVideo = function(page = 1) {
+  const url = `https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=${page}`
+  const options = {
+    url,
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${token}`
+    }
+  };
+
+  axios(options)
+    .then(res => {
+      const randomMovies = []
+      for (let i = 0; i < 5; i++) {
+        const randomIndex = Math.floor(Math.random() * res.data.results.length)
+        const randomMovie = res.data.results[randomIndex]
+        randomMovies.push({
+          title: randomMovie.title,
+          poster_path: randomMovie.poster_path
+        })
+      }
+
+      videos.value = randomMovies
+      currentPage.value = res.data.page
+      totalPages.value = res.data.total_pages
+    })
+    .catch(err => console.log(err))
+}
   </script>
   
   <style scoped>
