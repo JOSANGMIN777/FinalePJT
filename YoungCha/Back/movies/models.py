f9dbe3726bb1cf51348c1e088a9fe6e11a206c48
judonghyun <wn196785@gmail.com>
2023-11-23 13:53:10 +0900

    1123ju

--- YoungCha/Back/movies/models.py
+++ YoungCha/Back/movies/models.py
@@ -1,62 +1,57 @@
 from django.db import models
 from django.conf import settings
 
-class Movies(models.Model):
-    title = models.CharField(max_length=50)
-    overview = models.TextField()
+# class Movies(models.Model):
+#     title = models.CharField(max_length=50)
+#     overview = models.TextField()
 
-class MovieComment(models.Model):
-    movie = models.ForeignKey(Movies, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=100)
+# class MovieComment(models.Model):
+#     movie = models.ForeignKey(Movies, on_delete=models.CASCADE)
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     nickname = models.CharField(max_length=100)
 
-    user_rating = models.DecimalField(max_digits=3, decimal_places=2)  # DecimalField로 변경
+#     user_rating = models.DecimalField(max_digits=3, decimal_places=2)  # DecimalField로 변경
 
-    comments = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    liked_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_comments')  # ManyToManyField로 변경
+#     comments = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     liked_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_comments')  # ManyToManyField로 변경
 
-    def like_comment(self, user):
-        self.liked_users.add(user)
+#     def like_comment(self, user):
+#         self.liked_users.add(user)
 
-    def unlike_comment(self, user):
-        self.liked_users.remove(user)
+#     def unlike_comment(self, user):
+#         self.liked_users.remove(user)
 
-    def is_liked_by_user(self, user):
-        return self.liked_users.filter(pk=user.pk).exists()
+#     def is_liked_by_user(self, user):
+#         return self.liked_users.filter(pk=user.pk).exists()
     
 
-# class Genre(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     name = models.CharField(max_length=50)
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
 
 
-# class Movie(models.Model):
-    
-#     id = models.IntegerField(primary_key=True)
-#     title = models.CharField(max_length=100)
-#     release_date = models.DateField(null=True, blank=True)
-#     popularity = models.FloatField(null=True, blank=True)
-#     overview = models.TextField(null=True, blank=True)
-#     vote_average = models.FloatField(null=True, blank=True)
-#     vote_count = models.IntegerField(null=True, blank=True)
-#     poster_path = models.CharField(max_length=200, null=True, blank=True)
-
-#     video_url = models.CharField(max_length=200, null=True, blank=True)
-
-
-#     register_manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='register_movies', default=1)
-
-#     genre_check = models.ManyToManyField(Genre, related_name='genre_movies')
-
-#     like_movie_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies', blank=True)
-#     rates_users = models.ManyToManyField('accounts.User', related_name='user_rated_movie', through='Rate')
+class Movie(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=100)
+    release_date = models.DateField(null=True, blank=True)
+    popularity = models.FloatField(null=True, blank=True)
+    overview = models.TextField(null=True, blank=True)
+    vote_average = models.FloatField(null=True, blank=True)
+    vote_count = models.IntegerField(null=True, blank=True)
+    poster_path = models.CharField(max_length=200, null=True, blank=True)
+    genres = models.ManyToManyField(Genre)
+    video = models.CharField(max_length=200, null=True, blank=True)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='like_movies', blank=True,
+    )
 
     
 
 
-# class Rate(models.Model):
-#     rate_user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='user_rated')
-#     rate_movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-#     rate_score = models.FloatField()
+class Rate(models.Model):
+    rate_user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='user_rated')
+    rate_movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rate_score = models.FloatField()
--- YoungCha/Back/movies/serializers.py
+++ YoungCha/Back/movies/serializers.py
@@ -1,17 +1,37 @@
 from rest_framework import serializers
-from .models import MovieComment, Movies
+from .models import *
+from django.contrib.auth import get_user_model
 
-class MovieListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movies
-        fields = ('title', 'overview')
+# class MovieListSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Movies
+#         fields = ('title', 'overview')
 
-class MovieCommentListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MovieComment
-        fields = ('nickname', 'comments', 'created_at')  # 'comment' 필드가 'comments'로 변경되었습니다.
+# class MovieCommentListSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MovieComment
+#         fields = ('nickname', 'comments', 'created_at')  # 'comment' 필드가 'comments'로 변경되었습니다.
 
-class MovieCommentSerializer(serializers.ModelSerializer):
+# class MovieCommentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MovieComment
+#         fields = ('nickname', 'user_rating', 'comments', 'created_at', 'liked_users')  # 필드 목록에 'user_rating'과 'liked_users'를 추가하였습니다.
+
+
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
-        model = MovieComment
-        fields = ('nickname', 'user_rating', 'comments', 'created_at', 'liked_users')  # 필드 목록에 'user_rating'과 'liked_users'를 추가하였습니다.
+        model = Movie
+        fields = '__all__'
+
+
+class MovieSaveSerializer(serializers.ModelSerializer):
+    class UserReplySerializer(serializers.ModelSerializer):
+        class Meta:
+            model = get_user_model()
+            fields = ('id', 'username', 'nickname', 'age')
+    user = UserReplySerializer(read_only=True)
+
+    class Meta:
+        model = Movie
+        exclude = ('genres', )
+        
